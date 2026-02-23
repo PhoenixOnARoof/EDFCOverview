2daf73deff0ad226b6ef9ab4148d345bf0abf11a
DittoDuckPenguin <mohammadalexanderrain@gmail.com>
2026-02-23 12:30:31 +0330

    Discord API port

--- discord-api/app.js
+++ discord-api/app.js
@@ -11,7 +11,7 @@ import { getFakeProfile, getWikiItem } from './game.js';
 // Create an express app
 const app = express();
 // Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8087;
 // Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
