d7b65f027313758c8842df9192ab8d39ce3872fd
DittoDuckPenguin <mohammadalexanderrain@gmail.com>
2026-02-25 23:56:18 +0330

    🖥️ wip: Command interaction

--- discord-api/bot.js
+++ discord-api/bot.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, EmbedBuilder, GatewayIntentBits } from "discord.js";
 import { initializeCommands } from './utils/initializeCommands.js';
+import { InteractionResponseFlags } from 'discord-interactions';
 
 export const client = new Client({
     intents: [
@@ -22,6 +23,32 @@ client.on('ready', async () => {
 client.on('interactionCreate', async (interaction) => {
 
     console.log(interaction.commandName, interaction.commandType);
+
+    if (interaction.isCommand()) {
+
+        const { commandName } = interaction;
+        const command = client.commands.get(commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch (e) {
+            console.error('Command Error', error);
+
+            const embed = new EmbedBuilder()
+                .setColor(0xef4444)
+                .setTitle('Error')
+                .setDescription('An error occurred: ' + error.message);
+
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ embeds: [embed], flags: InteractionResponseFlags.EPHEMERAL });
+            } else
+                await interaction.reply({ embeds: [embed], flags: InteractionResponseFlags.EPHEMERAL });
+
+        }
+
+    }
 
 });
 
